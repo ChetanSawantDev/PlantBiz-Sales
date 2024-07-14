74fbfe8ec0d434983562d824403495cb6d51b8f0
ChetanSawant <chetansawant.dev@gmail.com>
2024-07-14 16:09:03 +0530

    Initial Commit for Sales Module

---
+++ src/main/java/com/plantbizsales/PlantBizSalesApplication.java
@@ -0,0 +1,13 @@
+package com.plantbizsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantBizSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantBizSalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plantbizsales/PlantBizSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantbizsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantBizSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
